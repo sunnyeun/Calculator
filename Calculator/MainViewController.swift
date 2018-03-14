aed826606d7b86a58b6b1f93a378a67f519bd8a4
Seung Hyun Eun <seunghyuneun@Seungs-MacBook-Pro-2.local>
2018-03-13 21:48:18 -0700

    Created buttons 1 and 2 Linked display label

--- Calculator/MainViewController.swift
+++ Calculator/MainViewController.swift
@@ -1,25 +1,12 @@
-//
-//  ViewController.swift
-//  Calculator
-//
-//  Created by Seung Hyun Eun on 3/13/18.
-//  Copyright © 2018 Sunny Eun. All rights reserved.
-//
-
 import UIKit
 
 class MainViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    @IBAction func tap(_ sender: UIButton) {
+        self.displayLabel!.text = sender.titleLabel?.text!
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
