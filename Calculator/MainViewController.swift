6e281355bbb39a1f158d3e6cb27dbad05bcf71cc
Seung Hyun Eun <seunghyuneun@Seungs-MacBook-Pro-2.local>
2018-03-20 21:43:53 -0700

    Complete Calculator Code

--- Calculator/MainViewController.swift
+++ Calculator/MainViewController.swift
@@ -2,11 +2,63 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    enum Operations {
+        case noop, add, subtract, multiply, divide, equal
+    }
+    
+    var operation: Operations = .noop
+    var accumulator = 0
+    var reset = false
+    
+    
     @IBOutlet weak var displayLabel: UILabel!
     
     @IBAction func tap(_ sender: UIButton) {
-        self.displayLabel!.text = sender.titleLabel?.text!
+        let symbol = sender.titleLabel?.text!
+        var currentDisplay = self.displayLabel.text!
+        
+        if let digit = Int(symbol!) {
+            if Int(currentDisplay) == 0 || reset {
+                currentDisplay = ""
+            }
+            
+            currentDisplay = currentDisplay + String(digit)
+            self.displayLabel.text = currentDisplay
+            reset = false
+            
+        } else if symbol == "C" {
+            self.displayLabel.text = "0"
+            accumulator = 0
+            operation = .noop
+            
+        } else {
+            switch (operation) {
+                case .add:
+                    accumulator = accumulator + Int(displayLabel.text!)!
+                case . subtract:
+                    accumulator = accumulator - Int(displayLabel.text!)!
+                case .multiply:
+                    accumulator = accumulator * Int(displayLabel.text!)!
+                case .divide:
+                    accumulator = accumulator / Int(displayLabel.text!)!
+            default:
+                accumulator = Int(displayLabel.text!)!
+            }
+            if symbol == "➕"{
+            operation = .add
+            } else if symbol == "➖"{
+                operation = .subtract
+            } else if symbol == "✖️"{
+                operation = .multiply
+            } else if symbol == "➗"{
+                operation = .divide
+            } else if symbol == "=" {
+                operation = .equal
+            }
+            reset = true
+            displayLabel.text = String(accumulator)
+            
+        }
     }
-    
 }
 
